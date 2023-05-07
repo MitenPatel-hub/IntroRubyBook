497edc4a27925003686a5e9246824bcf5939a271
Miten Patel <miten.patel@proton.em>
2023-05-07 15:52:31 -0500

    Initial Ch. 1 - Basics exercises commit

---
+++ ch_1_basics/basics_1.rb
@@ -0,0 +1,11 @@
+# My solution for Exercise 1 for Intro to Ruby - Basics Chapter
+
+# Add two strings together that, when concatenated, return a first and last name as a full name in one string.
+puts "Miten " + "Patel"
+
+first_name = 'Miten'
+last_name = 'Patel'
+
+puts first_name + ' ' + last_name
+
+puts "#{first_name} #{last_name}" 
